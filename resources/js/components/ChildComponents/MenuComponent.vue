8777a1feaa79391fc15222b4d23982a2e8c7bc92
tuankiet116 <kietdat1612000@gmail.com>
2022-10-11 00:44:57 +0700

    navbar update

--- resources/js/components/ChildComponents/MenuComponent.vue
+++ resources/js/components/ChildComponents/MenuComponent.vue
@@ -2,18 +2,17 @@
     <nav class="navbar" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
             <a class="navbar-item" href="https://bulma.io">
-                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+                <img src="/storage/default/brand.png">
             </a>
 
-            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
-                data-target="navbarBasicExample">
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menus">
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
             </a>
         </div>
 
-        <div id="navbarBasicExample" class="navbar-menu">
+        <div id="navbar-menus" class="navbar-menu">
             <div class="navbar-start">
                 <a class="navbar-item">
                     {{ $t('homepage') }}
@@ -59,15 +58,20 @@
                 </div>
             </div>
             <div class="navbar-end" v-else>
-                <div class="navbar-item">
-                    <div class="buttons">
-                        <a class="button">
-                            <strong>{{ user.name }}</strong>
-                        </a>
-                        <figure class="image">
+                <div class="navbar-item has-dropdown is-hoverable">
+
+                    <a class="navbar-link">
+                        <strong>{{ user.name }}</strong>
+                        <span>
                             <img class="is-rounded" src="/storage/default/avatar_default.png">
-                        </figure>
-                        <a class="button is-light">
+                        </span>
+                    </a>
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item">
+                            {{ $t('profile') }}
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">
                             {{ $t('logout') }}
                         </a>
                     </div>
@@ -87,4 +91,14 @@ export default {
 </script>
 
 <style scoped>
+.navbar {
+    background-color: rgba(24, 68, 151, 0.437);
+    padding: 0 10px;
+}
+
+.navbar img {
+    max-height: fit-content;
+    width: 60px;
+    height: 60px;
+}
 </style>
