2664ca3725ca5eeaa3bfc0737db73ae2e68146b9
kiet.do <kiet.do@pirago.work>
2022-10-25 17:26:34 +0700

    event

--- app/Events/EventListPost.php
+++ app/Events/EventListPost.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class EventListPost
+class EventListPost implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
